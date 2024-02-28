2da5f609f7544a0b43af0c489a388a2b7619af4d
kbventures <kmb_x@hotmail.com>
2024-02-28 17:26:44 -0500

    Worldcup sql queries

--- local/script-and-csv/queries.sh
+++ local/script-and-csv/queries.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+# Getting started
+# docker-compose up -d
+# docker-compose exec student_postgresql psql -U kmb worldcup
+# docker-compose exec student_postgresql bash
+
+PSQL="psql --username=kmb --dbname=worldcup --no-align --tuples-only -c"
+
+# Clear the terminal
+clear
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
@@ -8,34 +16,39 @@ echo -e "\nTotal number of goals in all games from winning teams:"
 echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
 
 echo -e "\nTotal number of goals in all games from both teams combined:"
-echo
+echo "$($PSQL "SELECT SUM(winner_goals + opponent_goals) FROM games")"
 
 echo -e "\nAverage number of goals in all games from the winning teams:"
-echo
+echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
 
 echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
-echo
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals),2) from games")"
 
 echo -e "\nAverage number of goals in all games from both teams:"
-echo
+echo "$($PSQL "SELECT AVG(winner_goals + opponent_goals) from games")"
 
 echo -e "\nMost goals scored in a single game by one team:"
-echo
+echo "$($PSQL "SELECT MAX(GREATEST(winner_goals, opponent_goals)) FROM games")"
 
 echo -e "\nNumber of games where the winning team scored more than two goals:"
-echo
+echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")"
 
+# DONE
 echo -e "\nWinner of the 2018 tournament team name:"
-echo
+echo "$($PSQL "SELECT winner FROM games WHERE year = 2018 AND round = 'Final'")"
 
+# DONE
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo
+echo "$($PSQL "SELECT team_name FROM ( SELECT winner AS team_name FROM games WHERE year = 2014 AND round = 'Eighth-Final' UNION SELECT opponent AS team_name FROM games WHERE year = 2014 AND round = 'Eighth-Final') AS combined_teams ORDER BY team_name;")"
 
+# DONE
 echo -e "\nList of unique winning team names in the whole data set:"
-echo
+echo "$($PSQL "SELECT DISTINCT winner FROM games ORDER BY winner")"
 
+# DONE
 echo -e "\nYear and team name of all the champions:"
-echo
+echo "$($PSQL "SELECT year, winner FROM games WHERE round = 'Final' ORDER BY year")"
 
+# DONE
 echo -e "\nList of teams that start with 'Co':"
-echo
+echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%'")"
